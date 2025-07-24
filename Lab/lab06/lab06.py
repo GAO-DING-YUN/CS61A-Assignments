c494d69163099a311229326f9f50cbcda56f24be
GAO-DING-YUN <gdy111111@outlook.com>
2025-07-24 22:09:19 +0800

    finish lab06

--- Lab/lab06/lab06.py
+++ Lab/lab06/lab06.py
@@ -7,6 +7,7 @@ class Transaction:
     def changed(self):
         """Return whether the transaction resulted in a changed balance."""
         "*** YOUR CODE HERE ***"
+        return self.after != self.before
 
     def report(self):
         """Return a string describing the transaction.
@@ -21,6 +22,10 @@ class Transaction:
         msg = 'no change'
         if self.changed():
             "*** YOUR CODE HERE ***"
+            if self.after > self.before:
+                return f'{self.id}: increased {self.before}->{self.after}'
+            else:
+                return f'{self.id}: decreased {self.before}->{self.after}'
         return str(self.id) + ': ' + msg
 
 class BankAccount:
@@ -67,21 +72,28 @@ class BankAccount:
     def __init__(self, account_holder):
         self.balance = 0
         self.holder = account_holder
+        self.transactions = []
 
     def deposit(self, amount):
         """Increase the account balance by amount, add the deposit
         to the transaction history, and return the new balance.
         """
+        before = self.balance
         self.balance = self.balance + amount
+        self.transactions += [Transaction(len(self.transactions), before, self.balance)]        
         return self.balance
 
     def withdraw(self, amount):
         """Decrease the account balance by amount, add the withdraw
         to the transaction history, and return the new balance.
         """
+        id = len(self.transactions)
         if amount > self.balance:
+            self.transactions += [Transaction(id, self.balance, self.balance)]
             return 'Insufficient funds'
+        before = self.balance
         self.balance = self.balance - amount
+        self.transactions += [Transaction(id, before, self.balance)]
         return self.balance
 
 
@@ -108,14 +120,14 @@ class Server:
         """Append the email to the inbox of the client it is addressed to.
             email is an instance of the Email class.
         """
-        ____.inbox.append(email)
+        self.clients[email.recipient_name].inbox.append(email)
 
     def register_client(self, client):
         """Add a client to the clients mapping (which is a 
         dictionary from client names to client instances).
             client is an instance of the Client class.
         """
-        ____[____] = ____
+        self.clients[client.name] = client
 
 class Client:
     """A client has a server, a name (str), and an inbox (list).
@@ -138,11 +150,11 @@ class Client:
         self.inbox = []
         self.server = server
         self.name = name
-        server.register_client(____)
+        server.register_client(self)
 
     def compose(self, message, recipient_name):
         """Send an email with the given message to the recipient."""
-        email = Email(message, ____, ____)
+        email = Email(message, self, recipient_name)
         self.server.send(email)
 
 
@@ -182,9 +194,11 @@ class Mint:
 
     def create(self, coin):
         "*** YOUR CODE HERE ***"
+        return coin(self.year)
 
     def update(self):
         "*** YOUR CODE HERE ***"
+        self.year = Mint.present_year
 
 class Coin:
     cents = None # will be provided by subclasses, but not by Coin itself
@@ -194,10 +208,155 @@ class Coin:
 
     def worth(self):
         "*** YOUR CODE HERE ***"
+        return self.cents + max(Mint.present_year - self.year - 50, 0)
 
 class Nickel(Coin):
     cents = 5
 
 class Dime(Coin):
     cents = 10
+
+def make_change(amount, coins):
+    """Return a list of coins that sum to amount, preferring the smallest coins
+    available and placing the smallest coins first in the returned list.
+
+    The coins argument is a dictionary with keys that are positive integer
+    denominations and values that are positive integer coin counts.
+
+    >>> make_change(2, {2: 1})
+    [2]
+    >>> make_change(2, {1: 2, 2: 1})
+    [1, 1]
+    >>> make_change(4, {1: 2, 2: 1})
+    [1, 1, 2]
+    >>> make_change(4, {2: 1}) == None
+    True
+
+    >>> coins = {2: 2, 3: 2, 4: 3, 5: 1}
+    >>> make_change(4, coins)
+    [2, 2]
+    >>> make_change(8, coins)
+    [2, 2, 4]
+    >>> make_change(25, coins)
+    [2, 3, 3, 4, 4, 4, 5]
+    >>> coins[8] = 1
+    >>> make_change(25, coins)
+    [2, 2, 4, 4, 5, 8]
+    """
+    if not coins:
+        return None
+    smallest = min(coins)
+    rest = remove_one(coins, smallest)
+    if amount < smallest:
+        return None
+    elif amount == smallest:
+        return [smallest]
+    else:
+        if make_change(amount - smallest, rest) == None:
+            return make_change(amount, rest)
+        else:
+            return [smallest] + make_change(amount - smallest, rest)
+
+def remove_one(coins, coin):
+    """Remove one coin from a dictionary of coins. Return a new dictionary,
+    leaving the original dictionary coins unchanged.
+
+    >>> coins = {2: 5, 3: 2, 6: 1}
+    >>> remove_one(coins, 2) == {2: 4, 3: 2, 6: 1}
+    True
+    >>> remove_one(coins, 6) == {2: 5, 3: 2}
+    True
+    >>> coins == {2: 5, 3: 2, 6: 1} # Unchanged
+    True
+    """
+    copy = dict(coins)
+    count = copy.pop(coin) - 1  # The coin denomination is removed
+    if count:
+        copy[coin] = count      # The coin denomination is added back
+    return copy
+
+class ChangeMachine:
+    """A change machine holds a certain number of coins, initially all pennies.
+    The change method adds a single coin of some denomination X and returns a
+    list of coins that sums to X. The machine prefers to return the smallest
+    coins available. The total value in the machine never changes, and it can
+    always make change for any coin (perhaps by returning the coin passed in).
+
+    The coins attribute is a dictionary with keys that are positive integer
+    denominations and values that are positive integer coin counts.
+
+    >>> m = ChangeMachine(2)
+    >>> m.coins == {1: 2}
+    True
+    >>> m.change(2)
+    [1, 1]
+    >>> m.coins == {2: 1}
+    True
+    >>> m.change(2)
+    [2]
+    >>> m.coins == {2: 1}
+    True
+    >>> m.change(3)
+    [3]
+    >>> m.coins == {2: 1}
+    True
+
+    >>> m = ChangeMachine(10) # 10 pennies
+    >>> m.coins == {1: 10}
+    True
+    >>> m.change(5) # takes a nickel & returns 5 pennies
+    [1, 1, 1, 1, 1]
+    >>> m.coins == {1: 5, 5: 1} # 5 pennies & a nickel remain
+    True
+    >>> m.change(3)
+    [1, 1, 1]
+    >>> m.coins == {1: 2, 3: 1, 5: 1}
+    True
+    >>> m.change(2)
+    [1, 1]
+    >>> m.change(2) # not enough 1's remaining; return a 2
+    [2]
+    >>> m.coins == {2: 1, 3: 1, 5: 1}
+    True
+    >>> m.change(8) # cannot use the 2 to make 8, so use 3 & 5
+    [3, 5]
+    >>> m.coins == {2: 1, 8: 1}
+    True
+    >>> m.change(1) # return the penny passed in (it's the smallest)
+    [1]
+    >>> m.change(9) # return the 9 passed in (no change possible)
+    [9]
+    >>> m.coins == {2: 1, 8: 1}
+    True
+    >>> m.change(10)
+    [2, 8]
+    >>> m.coins == {10: 1}
+    True
+
+    >>> m = ChangeMachine(9)
+    >>> [m.change(k) for k in [2, 2, 3]]
+    [[1, 1], [1, 1], [1, 1, 1]]
+    >>> m.coins == {1: 2, 2: 2, 3: 1}
+    True
+    >>> m.change(5) # Prefers [1, 1, 3] to [1, 2, 2] (more pennies)
+    [1, 1, 3]
+    >>> m.change(7)
+    [2, 5]
+    >>> m.coins == {2: 1, 7: 1}
+    True
+    """
+    def __init__(self, pennies):
+        self.coins = {1: pennies}
+
+    def change(self, coin):
+        """返回找零, 并从 self.coins 中移除."""
+        "*** YOUR CODE HERE ***"
+        if coin in self.coins:
+            self.coins[coin] += 1
+        else:
+            self.coins[coin] = 1
+        output_list = make_change(coin, self.coins)
+        for i in output_list:
+            self.coins = remove_one(self.coins, i)
+        return output_list
 
