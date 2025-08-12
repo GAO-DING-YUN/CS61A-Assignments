50c05ac5a79b40fbb320d5a836510dd717f0c8e4
GAO-DING-YUN <gdy111111@outlook.com>
2025-08-12 17:51:11 +0800

    project over

--- Project/scheme/questions.scm
+++ Project/scheme/questions.scm
@@ -6,9 +6,10 @@
 ;; Problem 15
 ;; Returns a list of two-element lists
 (define (enumerate s)
-  ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
+        (define (enhelper s cnt)
+        (if (null? s) '()
+        (cons (list cnt (car s))(enhelper (cdr s) (+ cnt 1)))))
+  (enhelper s 0))
   ; END PROBLEM 15
 
 ;; Problem 16
@@ -16,8 +17,11 @@
 ;; Merge two lists S1 and S2 according to ORDERED? and return
 ;; the merged lists.
 (define (merge ordered? s1 s2)
-  ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((ordered? (car s1) (car s2))
+        (cons (car s1) (merge ordered? (cdr s1) s2)))
+        (else (cons (car s2) (merge ordered? s1 (cdr s2)))))
   )
   ; END PROBLEM 16
 
--- Project/scheme/scheme_forms.py
+++ Project/scheme/scheme_forms.py
@@ -37,12 +37,21 @@ def do_define_form(expressions, env):
         validate_form(expressions, 2, 2) # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 4
         "*** YOUR CODE HERE ***"
+        value = scheme_eval(expressions.rest.first, env)
+        env.define(signature, value)
+        return signature
         # END PROBLEM 4
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
         "*** YOUR CODE HERE ***"
-        # END PROBLEM 10
+        symbol = signature.first
+        formals = signature.rest
+        validate_formals(formals)
+        body = expressions.rest
+        env.define(symbol, LambdaProcedure(formals, body, env))
+        return symbol
+    # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
         raise SchemeError('non-symbol: {0}'.format(bad_signature))
@@ -57,6 +66,7 @@ def do_quote_form(expressions, env):
     validate_form(expressions, 1, 1)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    return expressions.first
     # END PROBLEM 5
 
 def do_begin_form(expressions, env):
@@ -83,6 +93,8 @@ def do_lambda_form(expressions, env):
     validate_formals(formals)
     # BEGIN PROBLEM 7
     "*** YOUR CODE HERE ***"
+    body = expressions.rest
+    return LambdaProcedure(formals, body, env)
     # END PROBLEM 7
 
 def do_if_form(expressions, env):
@@ -116,6 +128,16 @@ def do_and_form(expressions, env):
     """
     # BEGIN PROBLEM 12
     "*** YOUR CODE HERE ***"
+    if expressions is nil:
+        return True
+    tmp = scheme_eval(expressions.first, env)
+    if is_scheme_true(tmp):
+        if expressions.rest is nil:
+            return tmp
+        else:
+            return do_and_form(expressions.rest, env)
+    else:
+        return tmp
     # END PROBLEM 12
 
 def do_or_form(expressions, env):
@@ -134,6 +156,16 @@ def do_or_form(expressions, env):
     """
     # BEGIN PROBLEM 12
     "*** YOUR CODE HERE ***"
+    if expressions is nil:
+        return False
+    tmp = scheme_eval(expressions.first, env)
+    if is_scheme_false(tmp):
+        if expressions.rest is nil:
+            return tmp
+        else:
+            return do_or_form(expressions.rest, env)
+    else:
+        return tmp
     # END PROBLEM 12
 
 def do_cond_form(expressions, env):
@@ -154,6 +186,9 @@ def do_cond_form(expressions, env):
         if is_scheme_true(test):
             # BEGIN PROBLEM 13
             "*** YOUR CODE HERE ***"
+            if clause.rest is nil:
+                return test
+            return eval_all(clause.rest, env)
             # END PROBLEM 13
         expressions = expressions.rest
 
@@ -178,6 +213,14 @@ def make_let_frame(bindings, env):
     names = vals = nil
     # BEGIN PROBLEM 14
     "*** YOUR CODE HERE ***"
+    while bindings != nil:
+        bin = bindings.first
+        validate_form(bin, 2, 2)
+        name, val = bin.first, scheme_eval(bin.rest.first, env)
+        vals = Pair(val, vals)
+        names = Pair(name, names)
+        bindings = bindings.rest
+    validate_formals(names)
     # END PROBLEM 14
     return env.make_child_frame(names, vals)
 
@@ -220,6 +263,7 @@ def do_mu_form(expressions, env):
     validate_formals(formals)
     # BEGIN PROBLEM 11
     "*** YOUR CODE HERE ***"
+    return MuProcedure(formals, expressions.rest)
     # END PROBLEM 11
 
 
